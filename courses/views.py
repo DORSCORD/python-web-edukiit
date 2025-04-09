27e39b100ac57dfa90d25d99ba4af91984201631
DORSCORD <dorscord356@gmail.com>
2025-04-09 19:04:03 +0200

    Add create, delete, change course

--- courses/views.py
+++ courses/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
+
+from .mixins import OwnerCourseEditMixin, OwnerCourseMixin
 
 # Create your views here.
-
 
 class HomeView(TemplateView):
 
     template_name = "base.html"
+
+class ManageCourseListView(OwnerCourseMixin, ListView):
+    template_name = 'courses/manage/course/list.html'
+    permission_required = 'course.view_course'
+
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
+    permission_required = 'courses.add_course'
+
+class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
+    permission_required = 'courses.change_course'
+
+class CourseDeleteView(OwnerCourseMixin, DeleteView):
+    template_name = 'courses/manage/course/delete.html'
+
     
