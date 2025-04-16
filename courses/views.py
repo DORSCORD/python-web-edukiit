f6338052fcdc4503ec0590af1c3b7404831ec83c
DORSCORD <dorscord356@gmail.com>
2025-04-16 19:06:19 +0200

    Add class ContentCreateUpdateView

--- courses/views.py
+++ courses/views.py
@@ -1,8 +1,16 @@
+from django.apps import apps
+from django.forms.models import modelform_factory
+from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import TemplateView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.base import TemplateResponseMixin, View
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
 
+from courses.models import Course
+
+from .forms import ModuleFormSet
 from .mixins import OwnerCourseEditMixin, OwnerCourseMixin
+from .models import Content, Module
 
 # Create your views here.
 
@@ -10,18 +18,62 @@ class HomeView(TemplateView):
 
     template_name = "base.html"
 
+
 class ManageCourseListView(OwnerCourseMixin, ListView):
+    permission_required = 'courses.view_course'
     template_name = 'courses/manage/course/list.html'
-    permission_required = 'course.view_course'
+
 
 class CourseCreateView(OwnerCourseEditMixin, CreateView):
     permission_required = 'courses.add_course'
 
+
 class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
     permission_required = 'courses.change_course'
+
 
 class CourseDeleteView(OwnerCourseMixin, DeleteView):
     permission_required = 'courses.delete_course'
     template_name = 'courses/manage/course/delete.html'
 
+
+class CourseModuleUpdateView(TemplateResponseMixin, View):
+    template_name = 'courses/manage/module/formset.html'
+    course = None
+
+    def get_formset(self, data=None):
+        return ModuleFormSet(instance=self.course, data=data)
+    
+    def dispatch(self, request, pk):
+        self.course = get_object_or_404(Course, id=pk, owner=request.user)
+        return super().dispatch(request, pk)
+    
+    def get(self, request, *args, **kwargs):
+        formset = self.get_formset()
+        return self.render_to_response({
+            'course': self.course,
+            'formset': formset,
+        })
+    
+    def post(self, request, *args, **kwargs):
+        formset = self.get_formset(data=request.POST)
+        if formset.is_valid():
+            formset.save()
+            return redirect('manage_course_list')
+        return self.render_to_response({
+            'course': self.course,
+            'formset': formset,
+        })
+
+class ContentCreateUpdateView(TemplateResponseMixin, View):
+    module = None
+    model = None
+    obj = None
+    template_name = 'courses/manage/content/form.html'
+
+    def get_model(self, model_name):
+        if model_name in ['text', 'file', 'video', 'image', 'url']:
+            return apps.get_model(app_label = 'courses', model_name=model_name)
+        return None
+    
     
