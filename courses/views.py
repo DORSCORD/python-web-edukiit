2eed2252da0251e8cbea64983b6c0704a87bd640
DORSCORD <dorscord356@gmail.com>
2025-05-14 17:34:55 +0200

    Fix bags

--- courses/views.py
+++ courses/views.py
@@ -139,12 +139,12 @@ class ModuleContentListView(TemplateResponseMixin, View):
 class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
     
     def post(self, request):
-        for id, order, in self.request_json.items():
+        for id, order in self.request_json.items():
             Module.objects.filter(
                 id=id,
                 course__owner=request.user
             ).update(order=order)
-        return self.render__json({'saved', 'OK'})
+        return self.render_json_response({'saved': 'OK'})
 
 class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
 
