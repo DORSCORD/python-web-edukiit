025bac188418ace8ea5e4e92602e2548d6991ee6
DORSCORD <dorscord356@gmail.com>
2025-06-11 19:04:29 +0200

    Fix bags

--- courses/api/url.py
+++ courses/api/url.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'courses'
 
 urlpatterns = [
-    path('subject/', views.SubjectListView.as_view(), name='subject_list'),
+    path('subjects/', views.SubjectListView.as_view(), name="subject_list"),
+    path('subjects/<pk>/', views.SubjectDetailView.as_view(), name="subject_detail"),
 ]
